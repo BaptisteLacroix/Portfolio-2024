32deb8cfc0c1c75f4305759ab5cc6fb868fa93b2
BaptisteLacroix <baptiste.lacroix03@gmail.com>
2024-09-22 17:55:23 +0200

    feat: improve Education & Experience Card size and position on mobile screen

--- src/modules/FormationComponent.tsx
+++ src/modules/FormationComponent.tsx
@@ -6,17 +6,17 @@ import {DownloadCVComponent} from "./DownloadCVComponent.tsx";
 
 export const FormationComponent: React.FC = () => {
     return (
-        <div className="flex flex-col lg:flex-row justify-center items-start lg:items-center p-6 min-h-[80vh]">
-            <div className="w-0 md:w-1/4"></div>
-            <div className="w-full lg:w-2/3 p-4 bg-white dark:bg-transparent">
+        <div className="flex flex-col lg:flex-row justify-center items-start lg:items-center p-6 sm:items-center">
+            <div className="w-1/4"></div>
+            <div className="w-full sm:w-3/4 lg:w-2/3 p-4 bg-white dark:bg-transparent">
                 <Card className="w-full lg dark:border-1 dark:border-amber-50 rounded-lg ml-0 sm:m-4 p-4">
-                    <CardHeader className="pb-0">
+                    <CardHeader className="pb-0 sm:justify-center">
                         <h1 className="text-xl lg:text-2xl font-bold text-gray-700 dark:text-blue-400">Education &
                             Experience</h1>
                     </CardHeader>
                     <Divider className="my-3 opacity-50"/>
                     <CardBody className="space-y-4">
-                        <div className="flex items-center space-x-3">
+                        <div className="flex items-center space-x-3 sm:justify-center">
                             <FlagIcon className="w-[30px] h-[30px]"/>
                             <div>
                                 <p className="text-gray-600 text-sm lg:text-base dark:text-white">
@@ -32,13 +32,13 @@ export const FormationComponent: React.FC = () => {
                             </div>
                         </div>
                     </CardBody>
-                    <CardFooter className="flex">
+                    <CardFooter className="flex sm:justify-center">
                         <DownloadCVComponent variant="solid"/>
                     </CardFooter>
                 </Card>
             </div>
             <ExperienceList/>
-            <div className="w-0 md:w-1/6"></div>
+            <div className="w-1/6"></div>
         </div>
     );
 };
